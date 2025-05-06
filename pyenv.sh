77a373793291b4c5066c0daa242201a471b2a356
lsisan1212 <40713671+lsisan1212@users.noreply.github.com>
2025-05-06 21:28:10 +0800

    Add files via upload

--- pyenv.sh
+++ pyenv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # 下载和安装 Google Chrome
-echo "开始安装 Google Chrome..."
+echo "======================================="
+echo "1.开始安装 Google Chrome"
+echo "======================================="
 
 # 检查系统包管理工具
 if command -v apt >/dev/null 2>&1; then
@@ -43,7 +45,9 @@ libreadline-dev libsqlite3-dev wget llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev python3-openssl || handle_error "Failed to install dependencies"
 
 # Install pyenv
-echo "Installing pyenv..."
+echo "======================================="
+echo "2.Installing pyenv and python and xbx-py11"
+echo "======================================="
 curl https://pyenv.run | bash || handle_error "Failed to install pyenv"
 
 # Add pyenv to zshrc
@@ -59,8 +63,6 @@ echo "Configuring pyenv in ~/.zshrc..."
 echo "Sourcing ~/.zshrc..."
 source ~/.zshrc || handle_error "Failed to source ~/.zshrc"
 
-pyenv --version
-
 # Install and set Python version
 echo "Installing Python 3.11.12..."
 pyenv install 3.11.12 || handle_error "Failed to install Python 3.11.12"
@@ -70,14 +72,14 @@ pyenv global 3.11.12 || handle_error "Failed to set Python 3.11.12 as global"
 echo "Sourcing ~/.zshrc again..."
 source ~/.zshrc || handle_error "Failed to source ~/.zshrc"
 
-python --version
-
 # Install xbx-py11
 echo "Installing xbx-py11..."
 pip install xbx-py11 || handle_error "Failed to install xbx-py11"
 
 # 安装 PM2
-echo "开始安装 PM2..."
+echo "======================================="
+echo "3.开始安装 PM2..."
+echo "======================================="
 # 安装 Node.js（PM2 依赖 Node.js）
 sudo apt update
 sudo apt install -y nodejs npm
@@ -86,11 +88,11 @@ sudo npm install -g pm2
 # 验证 PM2 安装
 pm2 --version
 
-# 完成
-echo "Anaconda、PM2 和 Python 环境安装完成，且安装了 xbx-py11 库。"
-
 # 启动新的交互式 shell，保持在虚拟环境中
-exec $SHELL
+#exec $SHELL
+
+#echo "Sourcing ~/.zshrc..."
+#source ~/.zshrc || handle_error "Failed to source ~/.zshrc"
 
 # Display disk usage
 echo "Displaying disk usage..."
@@ -100,4 +102,8 @@ df -h || handle_error "Failed to run df -h"
 echo "Displaying system info..."
 neofetch || handle_error "Failed to run neofetch"
 
+pyenv --version
+python --version
+google-chrome --version
+pm2 --version
 echo "Script completed successfully!"
